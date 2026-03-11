0828da548a77b1ab69d3b7fdb48ce3fc8f3bd79e
Kakarott78 <richard.nolhan87@gmail.com>
2026-03-11 14:40:53 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,35 +1,36 @@
+import os
 import discord
 from discord.ext import commands
-import random
-import os
 
+# Récupérer le token depuis Railway
 TOKEN = os.getenv("TOKEN")
 
+# Intents nécessaires
 intents = discord.Intents.default()
 intents.message_content = True
 
+# Création du bot
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-hugs = [
-"https://media.giphy.com/media/od5H3PmEG5EVq/giphy.gif",
-"https://media.giphy.com/media/l2QDM9Jnim1YVILXa/giphy.gif"
-]
-
-smiles = [
-"https://media.giphy.com/media/3o6Zt481isNVuQI1l6/giphy.gif",
-"https://media.giphy.com/media/26BRv0ThflsHCqDrG/giphy.gif"
-]
-
+# Quand le bot est prêt
 @bot.event
 async def on_ready():
     print(f"Connecté en tant que {bot.user}")
 
+# Commande ping
 @bot.command()
-async def hug(ctx):
-    await ctx.send(random.choice(hugs))
+async def ping(ctx):
+    await ctx.send("Pong ! 🏓")
 
+# Commande smile
 @bot.command()
 async def smile(ctx):
-    await ctx.send(random.choice(smiles))
+    await ctx.send("😊")
 
+# Commande hug
+@bot.command()
+async def hug(ctx, member: discord.Member):
+    await ctx.send(f"{ctx.author.mention} fait un câlin à {member.mention} 🤗")
+
+# Lancer le bot
 bot.run(TOKEN)
